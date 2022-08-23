d3e525e18ad5588eff5b10fffd24ec5a4584b78b
Edwin Chua <3dw1nchu4@gmail.com>
2022-08-23 23:14:47 +0800

    fix broken stuff after update, WIP

--- renderTabs.js
+++ renderTabs.js
@@ -1,28 +1,39 @@
 
 const { app, session } = require('electron')
-const TabGroup = require('electron-tabs');
+// const TabGroup = require('electron-tabs');
 
 const imageDirectory = 'file://' + __dirname + '/assets/images/';
 console.log(imageDirectory)
 
 let tabdata = require(__dirname + '/tabdata.json');
+const tabGroup = document.querySelector("tab-group");
+// let tabGroup = new TabGroup({
+//     newTab: {
+//         title: 'New Tab'
+//     }
+// });
 
-let tabGroup = new TabGroup({
-    newTab: {
-        title: 'New Tab'
-    }
-});
-
-let tabs = [];
-
+// let tabs = [];
+// tabGroup.setDefaultTab({
+//     title: "New Page",
+//     src: "path/to/new-page.html",
+//     active: true
+//   });
+// const tab = tabGroup.addTab({
+//     title: "electron-tabs on NPM",
+//     src: "https://www.npmjs.com/package/electron-tabs"
+//   });
 for (var i in tabdata) {
     let tab = {
         title: tabdata[i].title,
         src: tabdata[i].src,
         iconURL: imageDirectory + tabdata[i].iconFileName,
-        active: i == 0,
+        //active: i == 0,
         visible: true,
-        closable: false
+        closable: false,
+        ready:function(tab) {
+            tab.element.classList.add("etabs-tab");
+        }
     }
     tabGroup.addTab(tab);
 }
